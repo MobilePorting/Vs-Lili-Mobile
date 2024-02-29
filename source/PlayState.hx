bba53459a8526b6adc4a3972953ece064357c556
mcagabe19 <egzozu.be.bas@gmail.com>
2024-02-29 09:17:53 +0300

    oh

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3294,8 +3294,8 @@ class PlayState extends MusicBeatState
 
                         case 'Camera Flash':
                                 var val2:Float = Std.parseFloat(value2);
-                                var val1:FlxColor = value1;
-                                if(!val1.startsWith('0x')) val1 = '0xff' + val1;
+                                var val1:FlxColor = FlxColor.fromString(value1);
+                                //if(!val1.startsWith('0x')) val1 = '0xff' + val1;
                                 camOther.flash(val1, val2,null,true);
 				
 			case 'Red Vignette':
